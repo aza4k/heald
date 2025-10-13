1b43420d6d6d44b0bad204ef58d8636fb21c5194
Azamat Duysenbaev <aza4k>
2025-10-13 14:32:44 +0500

    I think this is last one

--- backend/settings.py
+++ backend/settings.py
@@ -67,7 +67,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     "default": dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
