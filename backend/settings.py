7938c22955b7bea52a491890b1fbec9229a6c3c2
Azamat Duysenbaev <aza4k>
2025-10-13 13:21:06 +0500

    Hosts

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-c(od$ckczq=)i=%^65n742-zi6pen_h!r&pb@979wjdb3s!&gf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*",'.vercel.app',]
 
 # Application definition
 
